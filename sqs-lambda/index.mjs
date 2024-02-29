06c52560bbf904705cb6c4ab494d28ed3f1c3613
Todd D Nestor <todd.nestor@gmail.com>
2024-02-29 13:35:49 -0800

    update the sqs lambda

--- sqs-lambda/index.mjs
+++ sqs-lambda/index.mjs
@@ -1,3 +1,8 @@
 export const handler = async (event) => {
   console.log('event:', event);
+  event.Records.forEach(({body}) => {
+    const data = JSON.parse(body);
+
+    console.log('data:', data);
+  });
 };
